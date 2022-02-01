c66f4b18dc8fc120be1a18ea42540e5dcecce36e
FrancoGT <francogt8000@gmail.com>
2022-02-01 11:43:13 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCPgPBJ7-xIY-zy6deoQDs-jnxdUvsmxLo",
+    authDomain: "notasalgoritmia.firebaseapp.com",
+    databaseURL: "https://notasalgoritmia-default-rtdb.firebaseio.com",
+    projectId: "notasalgoritmia",
+    storageBucket: "notasalgoritmia.appspot.com",
+    messagingSenderId: "1098215799530"
+  }
+};
